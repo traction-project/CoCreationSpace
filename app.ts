8513e5465d8291aca12191549cfe8068298c9b18
Thomas Röggla <tom@lichtfragmente.com>
2020-05-06 18:25:17 +0200

    added express setup files and app skeleton

---
+++ app.ts
@@ -0,0 +1,67 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { SESSION_SECRET, DB_URL } = process.env;
+
+import * as express from "express";
+import * as session from "express-session";
+import * as connectMongo from "connect-mongo";
+import * as logger from "morgan";
+import * as path from "path"
+import * as createError from "http-errors";
+import * as cookieParser from "cookie-parser";
+import { MongoClient } from "mongodb";
+
+import indexRouter from "./routes/index";
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+const MongoStore = connectMongo(session);
+
+app.use(session({
+  secret: SESSION_SECRET!,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false },
+  store: new MongoStore({ url: `${DB_URL!}/traction` })
+}));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+MongoClient.connect(DB_URL!, (err, client) => {
+  if (!err) {
+    console.log("DB connection established");
+    app.locals.db = client.db("traction");
+  } else {
+    console.error(err);
+  }
+});
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: express.Request, res: express.Response) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
