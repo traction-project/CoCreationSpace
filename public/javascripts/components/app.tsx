9e8337ae86867ea214520717a6d0c300eb840c34
Thomas Röggla <tom@lichtfragmente.com>
2020-05-08 00:43:08 +0200

    added Redux Provider component with store to App component

--- public/javascripts/components/app.tsx
+++ public/javascripts/components/app.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
+import { Provider } from "react-redux";
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
+
+import store from "../store";
 
 interface AppProps {
 }
 
 const App: React.FC<AppProps> = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          Hello World!
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path="/">
+            Hello World!
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
