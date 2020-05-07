8524d8d7b4f265141286752ab580f36a2bca4b84
Thomas Röggla <tom@lichtfragmente.com>
2020-05-08 00:42:39 +0200

    added basic redux store

---
+++ public/javascripts/store.ts
@@ -0,0 +1,20 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+
+import createRootReducer from "./reducers/index";
+
+// Combined application state
+export interface ApplicationState {
+}
+
+// This line is needed so the application works with Redux dev tools
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Initialise application store with root reducer and insert routing and thunk middlewares
+const store = createStore(
+  createRootReducer(),
+  undefined,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
