687cf3df9c80ea0cda3f6e8c5da361bdc869970d
Thomas Röggla <tom@lichtfragmente.com>
2020-05-08 00:42:53 +0200

    added index reducer which combines all reducers into one

---
+++ public/javascripts/reducers/index.ts
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+// Combine all reducers into a single reducer and return a function which takes
+// a history object as argument
+const createRootReducer = () => {
+  return combineReducers({
+  });
+};
+
+export default createRootReducer;
