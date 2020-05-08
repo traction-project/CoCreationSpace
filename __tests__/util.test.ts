bb4c16df196a3c94797da74611582e476aa8f263
Thomas Röggla <tom@lichtfragmente.com>
2020-05-08 14:41:03 +0200

    added more test cases for getFromEnvironment

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -12,4 +12,42 @@ describe("Utility function getFromEnvironment()", () => {
       getFromEnvironment("SOME_KEY");
     }).toThrow();
   });
+
+  it("should return the requested key if it's available", () => {
+    process.env = {
+      "SOME_KEY": "some_value"
+    };
+
+    expect(
+      getFromEnvironment("SOME_KEY")
+    ).toEqual([
+      "some_value"
+    ]);
+  });
+
+  it("should only return the requested keys", () => {
+    process.env = {
+      "SOME_KEY": "some_value",
+      "SOME_OTHER_KEY": "some_other_value",
+      "BLA": "yaddayadda"
+    };
+
+    expect(
+      getFromEnvironment("SOME_KEY", "BLA")
+    ).toEqual([
+      "some_value", "yaddayadda"
+    ]);
+  });
+
+  it("should throw an error if one of the requested keys is not available", () => {
+    process.env = {
+      "SOME_KEY": "some_value",
+      "SOME_OTHER_KEY": "some_other_value",
+      "BLA": "yaddayadda"
+    };
+
+    expect(() => {
+      getFromEnvironment("SOME_KEY", "BLA", "NO_SUCH_KEY");
+    }).toThrow();
+  });
 });
