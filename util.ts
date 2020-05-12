ae588a16015479a225b4401e3897a58030ee6038
Thomas Röggla <tom@lichtfragmente.com>
2020-05-12 17:17:35 +0200

    added function to upload files to S3

--- util.ts
+++ util.ts
@@ -1,3 +1,7 @@
+import * as aws from "aws-sdk";
+
+const [ BUCKET_NAME ] = getFromEnvironment("BUCKET_NAME");
+
 export function getFromEnvironment(...keys: Array<string>): Array<string> {
   return keys.reduce<Array<string>>((values, k) => {
     const value = process.env[k];
@@ -8,4 +12,24 @@ export function getFromEnvironment(...keys: Array<string>): Array<string> {
 
     return values.concat(value);
   }, []);
+}
+
+export function uploadToS3(filename: string, file: aws.S3.Body, bucket = BUCKET_NAME): Promise<void> {
+  const s3 = new aws.S3();
+
+  return new Promise((resolve, reject) => {
+    s3.upload({
+      Bucket: bucket, Key: filename, Body: file
+    }, {
+      partSize: 5 * 1024 * 1024, queueSize: 10
+    }, (err, data) => {
+      if (err) {
+        console.error("ERROR:", err);
+        reject(err);
+      } else {
+        console.log(data);
+        resolve();
+      }
+    });
+  });
 }
