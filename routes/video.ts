d8558d3dedd61834726181a8a2d3733bf4b6c82b
Thomas Röggla <tom@lichtfragmente.com>
2020-05-20 23:29:34 +0200

    starting video transcoding and saving video metadata to database

--- routes/video.ts
+++ routes/video.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import * as Busboy from "busboy";
 import { v4 as uuid4 } from "uuid";
 
-import { getExtension, uploadToS3, getFromEnvironment } from "../util";
+import Video from "../models/video";
+import { getExtension, uploadToS3, getFromEnvironment, encodeDash } from "../util";
 
-const [ BUCKET_NAME ] = getFromEnvironment("BUCKET_NAME");
+const [ BUCKET_NAME, ETS_PIPELINE ] = getFromEnvironment("BUCKET_NAME", "ETS_PIPELINE");
 const router = Router();
 
 router.post("/upload", (req, res) => {
@@ -13,7 +14,23 @@ router.post("/upload", (req, res) => {
   busboy.on("file", async (fieldname, file, filename, encoding, mimetype) => {
     try {
       const newName = uuid4() + getExtension(filename);
+
       await uploadToS3(newName, file, BUCKET_NAME);
+      const jobId = await encodeDash(ETS_PIPELINE, newName);
+
+      const video = new Video();
+      // TODO get user id
+      video.uploadedBy = "0";
+      video.title = filename;
+
+      if (jobId) {
+        video.transcodingJobId = jobId;
+        video.status = "processing";
+      } else {
+        video.status = "error";
+      }
+
+      await video.save();
 
       res.send({ status: "OK" });
     } catch {
