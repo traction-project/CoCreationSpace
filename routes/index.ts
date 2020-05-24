9305abe3700076cc02bff10ded1e9d8ebfb6d5ed
Thomas Röggla <tom@lichtfragmente.com>
2020-05-24 15:37:51 +0200

    implemented route POST /login

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import * as passport from "passport";
+
+import { User } from "../models/user";
 
 import APIRouter from "./api";
 import SNSRouter from "./sns";
@@ -12,6 +15,32 @@ router.use("/video", VideoRouter);
 
 router.get("/", (_, res) => {
   res.render("index", { title: "MediaVault" });
+});
+
+router.post("/login", (req, res, next) => {
+  const { username, password  } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).send({
+      status: "ERR",
+      message: "Insufficient parameters"
+    });
+  }
+
+  return passport.authenticate("local", { session: false }, (err: Error | null, user: User | undefined, msg: { message: string }) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      return res.send(user.getAuth());
+    }
+
+    res.status(401).send({
+      status: "ERR",
+      ...msg
+    });
+  })(req, res, next);
 });
 
 export default router;
