94a40dcc134be35bca5adf9e1cfb01b70863932b
Thomas Röggla <tom@lichtfragmente.com>
2020-05-27 01:34:02 +0200

    added reducers for login action creators

---
+++ public/javascripts/reducers/login.ts
@@ -0,0 +1,30 @@
+import { ActionHandler } from "../action_handler";
+import * as actions from "../actions/login";
+
+export interface LoginState {
+  loggedIn: boolean;
+  user?: { id: string, username: string };
+}
+
+export const initialState: LoginState = {
+  loggedIn: false
+};
+
+const actionHandler = new ActionHandler<LoginState>(initialState);
+
+actionHandler.addHandler("SET_LOGGED_IN_USER", (state, action: actions.SET_LOGGED_IN_USER) => {
+  const { payload } = action;
+
+  return {
+    loggedIn: true,
+    user: payload
+  };
+});
+
+actionHandler.addHandler("CLEAR_LOGGED_IN_USER", (state, action) => {
+  return {
+    loggedIn: false
+  };
+});
+
+export default actionHandler.getReducer();
