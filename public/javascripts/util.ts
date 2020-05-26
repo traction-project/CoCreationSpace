4f92d2d27aad4a4a104014afe3b2267c45ab0eaa
Thomas Röggla <tom@lichtfragmente.com>
2020-05-27 01:49:15 +0200

    added function for making POST requests with files in the body

--- public/javascripts/util.ts
+++ public/javascripts/util.ts
@@ -34,3 +34,26 @@ export interface PayloadAction<T extends string, U> extends BasicAction<T> {
  * @param A Actions used within action creator
  */
 export type AsyncAction<R, A extends Action> = ThunkAction<R, ApplicationState, void, A>;
+
+export function postFile(endpoint: string, file: File, onProgress: (e: ProgressEvent) => void, field = "file") {
+  const formData = new FormData();
+  formData.append(field, file);
+
+  const xhr = new XMLHttpRequest();
+  xhr.upload.onprogress = onProgress;
+
+  const promise = new Promise((resolve, reject) => {
+    xhr.upload.onloadend = () => {
+      resolve();
+    };
+
+    xhr.upload.onerror = (err) => {
+      reject(err);
+    };
+  });
+
+  xhr.open("POST", endpoint);
+  xhr.send(formData);
+
+  return promise;
+}
