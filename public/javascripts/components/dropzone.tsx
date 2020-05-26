d49086fc8b26b6a333a2ce9e1ff7ae6d9e86dae2
Thomas Röggla <tom@lichtfragmente.com>
2020-05-27 01:52:50 +0200

    added component for creating drop zones

---
+++ public/javascripts/components/dropzone.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { useState } from "react";
+import * as classNames from "classnames";
+
+interface DropzoneProps {
+  onFileDropped: (f: File) => void;
+  size: [number | string, number | string];
+}
+
+const Dropzone: React.FC<DropzoneProps> = (props) => {
+  const { onFileDropped, size: [width, height] } = props;
+  const [ dropzoneEntered, setDropzoneEntered ] = useState(false);
+
+  const parseFormData = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
+    console.log("File dropped");
+    const file = e.dataTransfer.files.item(0);
+
+    if (file) {
+      onFileDropped(file);
+    }
+  };
+
+  return (
+    <div>
+      <div
+        style={{ width, height }}
+        className={classNames("dropzone", { "dropzone-entered": dropzoneEntered })}
+        onDragEnter={() => setDropzoneEntered(true)}
+        onDragLeave={() => setDropzoneEntered(false)}
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={parseFormData}
+      >
+        <div>Drop a file here</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropzone;
