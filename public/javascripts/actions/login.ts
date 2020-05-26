31514fda811641679e5c983477ecca362cc3c018
Thomas Röggla <tom@lichtfragmente.com>
2020-05-27 01:33:40 +0200

    added action creators for login and logout

---
+++ public/javascripts/actions/login.ts
@@ -0,0 +1,29 @@
+import { ActionCreatorsMapObject } from "redux";
+import { BasicAction, PayloadAction } from "../util";
+
+export type SET_LOGGED_IN_USER = PayloadAction<"SET_LOGGED_IN_USER", { id: string, username: string }>;
+function setLoggedInUser(id: string, username: string): SET_LOGGED_IN_USER {
+  return {
+    type: "SET_LOGGED_IN_USER",
+    payload: {
+      id, username
+    }
+  };
+}
+
+export type CLEAR_LOGGED_IN_USER = BasicAction<"CLEAR_LOGGED_IN_USER">;
+function clearLoggedInUser(): CLEAR_LOGGED_IN_USER {
+  return {
+    type: "CLEAR_LOGGED_IN_USER"
+  };
+}
+
+export interface LoginActions extends ActionCreatorsMapObject {
+  setLoggedInUser: (id: string, username: string) => SET_LOGGED_IN_USER;
+  clearLoggedInUser: () => CLEAR_LOGGED_IN_USER;
+}
+
+export const actionCreators: LoginActions = {
+  setLoggedInUser,
+  clearLoggedInUser
+};
