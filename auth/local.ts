01a6e4552c2067f02e5955c095755f3d4539a305
Thomas Röggla <tom@lichtfragmente.com>
2020-05-26 22:30:03 +0200

    added functions to serialise and deserialise user to local auth

--- auth/local.ts
+++ auth/local.ts
@@ -1,14 +1,14 @@
 import * as passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 
-import User from "../models/user";
+import User, { User as UserSchema} from "../models/user";
 
 passport.use(new LocalStrategy((username, password, done) => {
   User.findOne({ username }).then((user) => {
     if (user && user.validatePassword(password)) {
       done(null, user);
     } else {
-      done(null, undefined, {
+      done(null, false, {
         message: "Username or password are invalid"
       });
     }
@@ -16,3 +16,13 @@ passport.use(new LocalStrategy((username, password, done) => {
     done
   );
 }));
+
+passport.serializeUser((user: UserSchema, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
