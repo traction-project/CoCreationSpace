de4ddb35be022ea8d21c56e02308dd1f93cca16f
Thomas Röggla <tom@lichtfragmente.com>
2020-05-26 22:00:16 +0200

    added POST /api/login which uses token-based login

--- routes/api/index.ts
+++ routes/api/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import * as passport from "passport";
+
+import { User as UserSchema } from "../../models/user";
 
 const router = Router();
 
@@ -6,6 +9,32 @@ router.get("/", (_, res) => {
   res.send({
     status: "OK"
   });
+});
+
+router.post("/login", (req, res, next) => {
+  const { username, password  } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).send({
+      status: "ERR",
+      message: "Insufficient parameters"
+    });
+  }
+
+  return passport.authenticate("local", { session: false }, (err: Error | null, user: UserSchema | undefined, msg: { message: string }) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (user) {
+      return res.send(user.getAuth());
+    }
+
+    res.status(401).send({
+      status: "ERR",
+      ...msg
+    });
+  })(req, res, next);
 });
 
 export default router;
