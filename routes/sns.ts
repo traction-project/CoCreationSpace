206e2619da1aea4d03710ab4485f040a91dde4f1
Thomas Röggla <tom@lichtfragmente.com>
2020-05-27 04:04:10 +0200

    updating video entry if transcoder notification is received

--- routes/sns.ts
+++ routes/sns.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
-import { getFromEnvironment } from "../util";
+import { getFromEnvironment, Range } from "../util";
 import { subscribeToSNSTopic, confirmSubscription } from "../util/sns";
+import Video from "../models/video";
 
 const [ SNS_ARN, SNS_ENDPOINT ] = getFromEnvironment("SNS_ARN", "SNS_ENDPOINT");
 subscribeToSNSTopic(SNS_ARN, `${SNS_ENDPOINT}/sns/receive`);
@@ -14,12 +15,39 @@ router.post("/receive", (req, res) => {
   } else {
     const topic = req.headers["x-amz-sns-topic-arn"];
 
-    switch (topic) {
-    default:
+    if (topic == SNS_ARN) {
+      const data = JSON.parse(req.body.Message);
+
+      if (data.state == "COMPLETED") {
+        insertVideoMetadata(data);
+      }
     }
   }
 
   res.send("");
 });
+
+export async function insertVideoMetadata(data: any) {
+  const { jobId, outputs } = data;
+  const thumbnailPattern: string = outputs[0].thumbnailPattern;
+
+  const thumbnails = Range(0, Math.floor(outputs[0].duration / 300) + 1).map((n) => {
+    return thumbnailPattern.replace(
+      "{count}",
+      (n + 1).toString().padStart(5, "0")
+    ) + ".png";
+  });
+
+  const video = await Video.findOne({ transcodingJobId: jobId });
+
+  if (video) {
+    video.status = "done";
+    video.thumbnails = thumbnails;
+    video.resolutions = outputs.filter((o: any) => o.height).map((o: any) => o.height);
+    video.duration = outputs[0].duration;
+
+    video.save();
+  }
+}
 
 export default router;
