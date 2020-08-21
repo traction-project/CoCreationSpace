11afd5e72ab36f26da50e3fc95edd5bf1bfd96bd
Thomas Röggla <tom@lichtfragmente.com>
2020-08-21 15:39:07 +0200

    added button to start video recording and placing output in hidden field

--- public/javascripts/components/video_recorder.tsx
+++ public/javascripts/components/video_recorder.tsx
@@ -1,13 +1,26 @@
 import * as React from "react";
+import { useRef } from "react";
 
 interface VideoRecorderProps {
 }
 
 const VideoRecorder: React.FC<VideoRecorderProps> = () => {
+  const fileInput = useRef<HTMLInputElement>(null);
+
   return (
     <div>
       <h1 className="title">Record Video</h1>
-      <input type="file" accept="video/*" capture="environment" />
+      <input
+        ref={fileInput}
+        style={{ display: "none" }}
+        type="file"
+        accept="video/*"
+        capture="environment"
+      />
+
+      <button className="button is-info" onClick={() => fileInput.current?.click()}>
+        Record
+      </button>
     </div>
   );
 };
