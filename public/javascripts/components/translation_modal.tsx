03acc37e94a2bb11e70ab590acbe52c87e667af1
Thomas Röggla <t.roggla@cwi.nl>
2020-10-26 22:28:05 +0100

    added component for automatic translation via a modal

---
+++ public/javascripts/components/translation_modal.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { useState } from "react";
+
+import LanguageSelector from "./language_selector";
+
+interface TranslationModalProps {
+  id: string;
+  onClose: () => {};
+}
+
+const TranslationModal: React.FC<TranslationModalProps> = (props) => {
+  const { id, onClose } = props;
+  const [ targetLanguage, setTargetLanguage ] = useState<string>("");
+
+  const onDone = async () => {
+    await fetch(`/translate/${id}/${targetLanguage}`, { method: "POST" });
+  };
+
+  return (
+    <div className="modal is-active">
+      <div className="modal-background" onClick={onClose} />
+      <div className="modal-content">
+        <div className="box">
+          <div className="field">
+            <label className="label">Language</label>
+            <div className="control">
+              <LanguageSelector
+                value={targetLanguage}
+                onChange={(e) => setTargetLanguage(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-link" onClick={onDone}>Translate</button>
+            </div>
+            <div className="control">
+              <button className="button is-link is-light" onClick={onClose}>Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TranslationModal;
