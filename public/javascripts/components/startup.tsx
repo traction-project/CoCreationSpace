5d8e9cf6bc2229c6a7691b6c9b592d970983f5f0
Thomas Röggla <tom@lichtfragmente.com>
2020-10-28 17:16:22 +0100

    Startup component accepts now a condition prop

--- public/javascripts/components/startup.tsx
+++ public/javascripts/components/startup.tsx
@@ -1,44 +1,16 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import { Dispatch, bindActionCreators } from "redux";
-import { connect } from "react-redux";
 
-import { actionCreators as loginActionCreators, LoginActions } from "../actions/login";
-import { LoginState } from "../reducers/login";
-import { ApplicationState } from "../store";
-import { verifyLoginStatus } from "../util";
-
-interface StartupActionProps {
-  loginActions: LoginActions;
+interface StartupProps {
+  condition: () => Promise<void>;
 }
-
-interface StartupConnectedProps {
-  login: LoginState;
-}
-
-type StartupProps = StartupActionProps & StartupConnectedProps;
 
 const Startup: React.FC<StartupProps> = (props) => {
   const [ appInitialised, setAppInitialised ] = useState(false);
 
   useEffect(() => {
-    verifyLoginStatus().then((loginStatus) => {
-      if (loginStatus == props.login.loggedIn) {
-        setAppInitialised(true);
-        return;
-      }
-
-      if (loginStatus == true) {
-        fetch("/users/profile").then((res) => {
-          return res.json();
-        }).then(({ id, username, image }) => {
-          props.loginActions.setLoggedInUser(id, username, image);
-          setAppInitialised(true);
-        });
-      } else {
-        props.loginActions.clearLoggedInUser();
-        setAppInitialised(true);
-      }
+    props.condition().then(() => {
+      setAppInitialised(true);
     });
   }, []);
 
@@ -51,16 +23,4 @@ const Startup: React.FC<StartupProps> = (props) => {
   );
 };
 
-function mapStateToProps(state: ApplicationState): StartupConnectedProps {
-  return {
-    login: state.login
-  };
-}
-
-function mapDispatchToProps(dispatch: Dispatch) {
-  return {
-    loginActions: bindActionCreators(loginActionCreators, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Startup);
+export default Startup;
