7788469c3b7d6d3e1dbc2960963dbbd509ed3590
Thomas Röggla <tom@lichtfragmente.com>
2020-10-29 16:00:46 +0100

    auth/local.ts now exports a function which has to be called

--- auth/local.ts
+++ auth/local.ts
@@ -4,30 +4,32 @@ import { Strategy as LocalStrategy } from "passport-local";
 import { UserInstance } from "../models/users";
 import { db } from "../models";
 
-passport.use(new LocalStrategy((username, password, done) => {
-  const { Users } = db.getModels();
+export default function setup() {
+  passport.use(new LocalStrategy((username, password, done) => {
+    const { Users } = db.getModels();
 
-  Users.findOne( { where: { username } }).then((user: UserInstance) => {
-    if (user && user.validatePassword(password)) {
+    Users.findOne( { where: { username } }).then((user: UserInstance) => {
+      if (user && user.validatePassword(password)) {
+        done(null, user);
+      } else {
+        done(null, false, {
+          message: "Username or password are invalid"
+        });
+      }
+    }).catch(
+      done
+    );
+  }));
+
+  passport.serializeUser((user: UserInstance, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id: string, done) => {
+    const { Users } = db.getModels();
+
+    Users.findOne({where : { id }}).then((user: UserInstance) => {
       done(null, user);
-    } else {
-      done(null, false, {
-        message: "Username or password are invalid"
-      });
-    }
-  }).catch(
-    done
-  );
-}));
-
-passport.serializeUser((user: UserInstance, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id: string, done) => {
-  const { Users } = db.getModels();
-
-  Users.findOne({where : { id }}).then((user: UserInstance) => {
-    done(null, user);
-  }).catch(done);
-});
+    }).catch(done);
+  });
+}
