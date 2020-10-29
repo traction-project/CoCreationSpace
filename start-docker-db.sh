e6b71e7ca78df2796e072cae86276344412691b6
Thomas Röggla <tom@lichtfragmente.com>
2020-10-29 16:26:01 +0100

    removed obsolete scripts for running seed migrations

--- start-docker-db.sh
+++
@@ -1,8 +0,0 @@
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
-    echo "-- First container startup --"
-    npx sequelize db:seed:all
-else
-    echo "-- Not first container startup --"
-fi
