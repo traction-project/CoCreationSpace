e95b33448a895b98d007523dbc09294d4b93df28
Thomas Röggla <tom@lichtfragmente.com>
2020-10-31 01:33:44 +0100

    perform prefix matching on detected lang code, otherwise default to en

--- public/javascripts/components/language_switcher.tsx
+++ public/javascripts/components/language_switcher.tsx
@@ -9,27 +9,36 @@ const availableTranslations: { [key: string]: string } = {
   es: "Español"
 };
 
+function processDetectedLanguage() {
+  const languageCodes = Object.keys(availableTranslations);
+  const matchingLanguageCode = languageCodes.find((t) => i18n.language.startsWith(t));
+
+  if (matchingLanguageCode) {
+    return matchingLanguageCode;
+  } else {
+    return languageCodes[0];
+  }
+}
+
 interface LanguageSwitcherProps {
   onLangaugeChanged?: (selectedLanguage: string, languageName: string) => void;
 }
 
 const LanguageSwitcher: React.FC<LanguageSwitcherProps> = (props) => {
   const { onLangaugeChanged } = props;
-  const [ language, setLanguage ] = useState(
-    i18n.language || Object.values(availableTranslations)[0]
-  );
+  const [ languageCode, setLanguageCode ] = useState(processDetectedLanguage());
 
   const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const code = e.target.value;
 
-    setLanguage(code);
+    setLanguageCode(code);
     i18n.changeLanguage(code);
     onLangaugeChanged?.(code, availableTranslations[code]);
   };
 
   return (
     <div className="select">
-      <select value={language} onChange={onChange}>
+      <select value={languageCode} onChange={onChange}>
         {Object.entries(availableTranslations).map(([languageCode, languageName]) => {
           return (
             <option key={languageCode} value={languageCode}>{languageName}</option>
