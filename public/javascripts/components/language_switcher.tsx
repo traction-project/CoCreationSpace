01214e1b0ade1c8365a23c8d8d6c5bf0c5e31f36
Thomas Röggla <tom@lichtfragmente.com>
2020-10-31 01:28:04 +0100

    initialising LanguageSwitcher to detected langauge

--- public/javascripts/components/language_switcher.tsx
+++ public/javascripts/components/language_switcher.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import i18n from "../i18n";
 
-const availableLanguages: { [key: string]: string } = {
+const availableTranslations: { [key: string]: string } = {
   en: "English",
   de: "Deutsch",
   es: "Español"
@@ -15,20 +15,22 @@ interface LanguageSwitcherProps {
 
 const LanguageSwitcher: React.FC<LanguageSwitcherProps> = (props) => {
   const { onLangaugeChanged } = props;
-  const [ language, setLanguage ] = useState(Object.values(availableLanguages)[0]);
+  const [ language, setLanguage ] = useState(
+    i18n.language || Object.values(availableTranslations)[0]
+  );
 
   const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const code = e.target.value;
 
     setLanguage(code);
     i18n.changeLanguage(code);
-    onLangaugeChanged?.(code, availableLanguages[code]);
+    onLangaugeChanged?.(code, availableTranslations[code]);
   };
 
   return (
     <div className="select">
       <select value={language} onChange={onChange}>
-        {Object.entries(availableLanguages).map(([languageCode, languageName]) => {
+        {Object.entries(availableTranslations).map(([languageCode, languageName]) => {
           return (
             <option key={languageCode} value={languageCode}>{languageName}</option>
           );
