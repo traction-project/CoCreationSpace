8af6563616b03c8730a1e73efe7292a999ad17f8
Thomas Röggla <tom@lichtfragmente.com>
2020-11-04 16:52:14 +0100

    added migration for creating a table named interests

---
+++ sequelize/migrations/20201104154949-add-interests-table.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//  DB table name
+const TABLE_NAME = "interests";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Model attributtes
+    const attributes = {
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      },
+      user_id : {
+        type: Sequelize.DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false
+      },
+      topic_id: {
+        type: Sequelize.DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false
+      }
+    };
+
+    return queryInterface.createTable(TABLE_NAME, attributes);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable(TABLE_NAME);
+  }
+};
