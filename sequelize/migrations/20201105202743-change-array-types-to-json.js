fc89f3ceeae4e987889faee6d92aa9ba5e0b2769
Thomas Röggla <t.roggla@cwi.nl>
2020-11-06 12:09:03 +0100

    added proper casts to/from json to newest multimedia migration

--- sequelize/migrations/20201105202743-change-array-types-to-json.js
+++ sequelize/migrations/20201105202743-change-array-types-to-json.js
@@ -3,21 +3,21 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn("multimedia", "thumbnails", {
-      type: Sequelize.DataTypes.JSON
+      type: "JSON USING to_json(thumbnails)::JSON",
     });
 
     await queryInterface.changeColumn("multimedia", "resolutions", {
-      type: Sequelize.DataTypes.JSON
+      type: "JSON USING to_json(resolutions)::JSON",
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn("multimedia", "thumbnails", {
-      type: Sequelize.DataTypes.ARRAY
+      type: "VARCHAR[] USING translate(thumbnails::text, '[]', '{}')::VARCHAR[]"
     });
 
     await queryInterface.changeColumn("multimedia", "resolutions", {
-      type: Sequelize.DataTypes.ARRAY
+      type: "INTEGER[] USING translate(resolutions::text, '[]', '{}')::INT[]"
     });
   }
 };
