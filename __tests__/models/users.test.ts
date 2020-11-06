86aa8876f4b5661b62146729d8b2abaa7bb2482c
Thomas Röggla <tom@lichtfragmente.com>
2020-11-06 02:42:31 +0100

    added some initial test cases for users model

---
+++ __tests__/models/users.test.ts
@@ -0,0 +1,39 @@
+import { Sequelize } from "sequelize";
+
+process.env["SESSION_SECRET"] = "sessionsecret";
+const sequelize = new Sequelize("sqlite::memory:", { logging: false });
+
+import { db } from "../../models";
+
+describe("Users model", () => {
+  beforeAll(async () => {
+    await db.createDB(sequelize);
+  });
+
+  it("should create a new user with just a username", async () => {
+    const { Users } = db.getModels();
+
+    const user = await Users.build({
+      username: "test"
+    }).save();
+
+    expect(user.username).toEqual("test");
+
+    expect(user.id).toBeDefined();
+    expect(user.createdAt).toBeDefined();
+    expect(user.updatedAt).toBeDefined();
+
+    expect(user.createdAt).toEqual(user.updatedAt);
+  });
+
+  it("username cannot be empty", async () => {
+    const { Users } = db.getModels();
+
+    try {
+      await Users.build({ username: "" }).save();
+      fail();
+    } catch (e) {
+      expect(e).toBeDefined();
+    }
+  });
+});
