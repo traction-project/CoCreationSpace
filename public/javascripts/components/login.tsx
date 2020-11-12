ca52f6a36c6065b4670d5ce7a4d5a07d3759ad17
Thomas Röggla <tom@lichtfragmente.com>
2020-11-12 23:33:30 +0100

    re-implemented login form using react-hook-form

--- public/javascripts/components/login.tsx
+++ public/javascripts/components/login.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { useState } from "react";
 import { Dispatch, bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
 
 import { actionCreators as loginActionCreators, LoginActions } from "../actions/login";
 import { LoginState } from "../reducers/login";
@@ -20,17 +20,17 @@ interface LoginConnectedProps {
 type LoginProps = LoginActionProps & LoginConnectedProps;
 
 const Login: React.FC<LoginProps> = (props) => {
+  const { register, watch, handleSubmit } = useForm();
   const { t } = useTranslation();
   const history = useHistory();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const onSubmit = () => {
+  const onSubmit = handleSubmit(({ username, password }) => {
     props.loginActions.performLogin(username, password, () => {
       history.push("/");
     });
-  };
+  });
+
+  const submitDisabled = (watch("username") || "").length == 0 || (watch("password") || "").length == 0;
 
   return (
     <section className="hero is-fullheight-with-navbar">
@@ -38,27 +38,41 @@ const Login: React.FC<LoginProps> = (props) => {
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-6-tablet is-5-desktop is-5-widescreen">
-              <form action="" className="box">
+              <form onSubmit={onSubmit} className="box">
                 <div className="field">
-                  <label htmlFor="" className="label">Username</label>
+                  <label htmlFor="" className="label">{t("Username")}</label>
                   <div className="control has-icons-left">
-                    <input type="text" placeholder="Username" className="input" required={true} />
+                    <input
+                      type="text"
+                      placeholder={t("Username")}
+                      name="username"
+                      className="input"
+                      required={true}
+                      ref={register}
+                    />
                     <span className="icon is-small is-left">
                       <i className="fa fa-envelope" />
                     </span>
                   </div>
                 </div>
                 <div className="field">
-                  <label htmlFor="" className="label">Password</label>
+                  <label htmlFor="" className="label">{t("Password")}</label>
                   <div className="control has-icons-left">
-                    <input type="password" placeholder="Password" className="input" required={true} />
+                    <input
+                      type="password"
+                      placeholder={t("Password")}
+                      name="password"
+                      className="input"
+                      required={true}
+                      ref={register}
+                    />
                     <span className="icon is-small is-left">
                       <i className="fa fa-lock"></i>
                     </span>
                   </div>
                 </div>
                 <div className="field">
-                  <button className="button is-success">
+                  <button type="submit" disabled={submitDisabled} className="button is-success">
                     Login
                   </button>
                 </div>
