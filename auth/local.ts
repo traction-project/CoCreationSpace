3d2c01e5cf118350665e187b003716c2e73a5b16
Thomas Röggla <t.roggla@cwi.nl>
2020-11-12 16:03:13 +0100

    added JWT authentication strategy

--- auth/local.ts
+++ auth/local.ts
@@ -1,8 +1,12 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
 
+import { getFromEnvironment } from "../util";
 import { UserInstance } from "../models/users";
 import { db } from "../models";
+
+const [ SESSION_SECRET ] = getFromEnvironment("SESSION_SECRET");
 
 export default async function setup() {
   passport.use(new LocalStrategy((username, password, done) => {
@@ -19,6 +23,22 @@ export default async function setup() {
     }).catch(
       done
     );
+  }));
+
+  passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: SESSION_SECRET
+  }, (token, done) => {
+    const { Users } = db.getModels();
+
+    Users.findByPk(token.id).then((user) => {
+      if (user) {
+        done(null, user);
+      } else {
+        console.log("user not found");
+        done(new Error("Token invalid"));
+      }
+    });
   }));
 
   passport.serializeUser((user: UserInstance, done) => {
