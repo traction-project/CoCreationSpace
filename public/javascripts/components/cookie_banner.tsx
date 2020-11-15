43a16e0fb499f956db57227dabfde0dbb7aac941
Thomas Röggla <tom@lichtfragmente.com>
2020-11-15 04:40:40 +0100

    added translation strings for cookie banner

--- public/javascripts/components/cookie_banner.tsx
+++ public/javascripts/components/cookie_banner.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useState } from "react";
+import { useTranslation, Trans } from "react-i18next";
 
 function getCookieConsentStatus() {
   const hasConsented = localStorage.getItem("gdprCookieConsent");
@@ -12,6 +13,7 @@ function getCookieConsentStatus() {
 }
 
 const CookieBanner: React.FC = () => {
+  const { t } = useTranslation();
   const [ cookiesConsented, setCookiesConsented ] = useState(getCookieConsentStatus());
 
   if (cookiesConsented) {
@@ -28,12 +30,14 @@ const CookieBanner: React.FC = () => {
       <div className="column cookie-banner is-3-desktop is-3-widescreen is-6-tablet">
         <div className="box">
           <p>
-            The cookies on this website are used to personalize content, store session data
-            and improve your browsing experience. We do not share your data with any third
-            party.
+            <Trans i18nKey="cookie-notice">
+              The cookies on this website are used to personalize content, store session data
+              and improve your browsing experience. We do not share your data with any third
+              party.
+            </Trans>
           </p>
           <button className="button is-info" onClick={onAccept}>
-            Accept
+            {t("Accept")}
           </button>
         </div>
       </div>
