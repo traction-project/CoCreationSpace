d47a84f079d4300ffd0f6f2a422020977b88d89b
Thomas Röggla <tom@lichtfragmente.com>
2020-11-15 03:36:01 +0100

    added file upload button to Dropzone component

--- public/javascripts/components/dropzone.tsx
+++ public/javascripts/components/dropzone.tsx
@@ -35,6 +35,26 @@ const Dropzone: React.FC<DropzoneProps> = (props) => {
         onDrop={parseFormData}
       >
         <div>{t("Drop a file here")}</div>
+
+        <div className="file">
+          <label className="file-label">
+            <input
+              className="file-input"
+              type="file"
+              name="resume"
+              onChange={(e) => e.target.files && handleButtonClick(e.target.files)}
+            />
+            <span className="file-cta">
+              <span className="file-icon">
+                <i className="fas fa-upload"></i>
+              </span>
+              &nbsp;
+              <span className="file-label">
+                {t("Choose a file")}…
+              </span>
+            </span>
+          </label>
+        </div>
       </div>
     </React.Fragment>
   );
