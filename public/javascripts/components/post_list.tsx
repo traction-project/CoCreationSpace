7dbafeb3c3a0a2bd7c97d239141ee7ddf1ba45d8
Thomas Röggla <tom@lichtfragmente.com>
2020-11-16 03:14:59 +0100

    wrapped PostList into container element

--- public/javascripts/components/post_list.tsx
+++ public/javascripts/components/post_list.tsx
@@ -91,78 +91,82 @@ const PostList: React.FC<PostListProps> = ({endpoint}) => {
   };
 
   return (
-    <React.Fragment>
-      <div>
-        <Filter searchValueChange={handleChange}></Filter>
-      </div>
-      <div className="columns" style={{ marginTop: 15 }}>
-        <div className="column is-8 is-offset-1">
-          <div>
-            {(filteredPosts) ? (
-              filteredPosts.map((post, index) => {
-                return (
-                  <div key={index}>
-                    <Link to={`/post/${post.id}`}>
-                      <div className="list-item">
-                        <div className="box" style={{width: "100%"}}>
-                          <article className="media">
-                            <UserLogo user={post.user}></UserLogo>
-                            <div className="media-content">
-                              <div className="content">
-                                <p>
-                                  <strong className="post-title">{post.title ? post.title : "Post"}</strong>
-                                  <small className="list-item__date">{post.createdAt && new Date(post.createdAt).toLocaleDateString()}</small>
-                                  <br />
-                                  <br />
-                                  {post.dataContainer?.text_content}
-                                </p>
-                              </div>
-                              { (post.dataContainer && post.dataContainer.multimedia && post.dataContainer.multimedia.length > 0) ?
-                                <div className="list-item__files">
-                                  {post.dataContainer.multimedia.map((multimedia, index) => {
-                                    return (
-                                      <div key={index}>
-                                        <figure className="image is-24x24 list-item__files-item">
-                                          <img src="/images/docs.png" />
-                                        </figure>
-                                      </div>
-                                    );
-                                  })}
-                                </div>
-                                : null}
-                            </div>
-                            <div className="media-right number-comments">
-                              <i className="far fa-comment"></i>
-                              <p>{(post.comments && post.comments.length > 0) ? post.comments.length : 0} {(post.comments && post.comments.length == 1) ? t("Comment") : t("Comments") }</p>
-                            </div>
-                          </article>
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })
-            ) : (
-              <p>{t("This user has not posts")}</p>
-            )}
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <Filter searchValueChange={handleChange}></Filter>
           </div>
         </div>
-        <div className="column is-2">
-          <ul className="lateral-menu">
-            <li><button className="btn" onClick={handleClickButtonNewPost}>{t("New Post")}</button></li>
-            <li className="lateral-menu__item" onClick={handleClickAllPosts}>{t("All posts")}</li>
-            <hr />
-            {(tags) ? (
-              tags.map((tag, index) => {
-                return (<li key={index} className="tag" onClick={() => handleClickTag(tag)}>{tag.tag_name}</li>);
-              })
-            ) : (
-              null
-            )}
-          </ul>
+        <div className="columns" style={{ marginTop: 15 }}>
+          <div className="column is-8 is-offset-1">
+            <div>
+              {(filteredPosts) ? (
+                filteredPosts.map((post, index) => {
+                  return (
+                    <div key={index}>
+                      <Link to={`/post/${post.id}`}>
+                        <div className="list-item">
+                          <div className="box" style={{width: "100%"}}>
+                            <article className="media">
+                              <UserLogo user={post.user}></UserLogo>
+                              <div className="media-content">
+                                <div className="content">
+                                  <p>
+                                    <strong className="post-title">{post.title ? post.title : "Post"}</strong>
+                                    <small className="list-item__date">{post.createdAt && new Date(post.createdAt).toLocaleDateString()}</small>
+                                    <br />
+                                    <br />
+                                    {post.dataContainer?.text_content}
+                                  </p>
+                                </div>
+                                { (post.dataContainer && post.dataContainer.multimedia && post.dataContainer.multimedia.length > 0) ?
+                                  <div className="list-item__files">
+                                    {post.dataContainer.multimedia.map((multimedia, index) => {
+                                      return (
+                                        <div key={index}>
+                                          <figure className="image is-24x24 list-item__files-item">
+                                            <img src="/images/docs.png" />
+                                          </figure>
+                                        </div>
+                                      );
+                                    })}
+                                  </div>
+                                  : null}
+                              </div>
+                              <div className="media-right number-comments">
+                                <i className="far fa-comment"></i>
+                                <p>{(post.comments && post.comments.length > 0) ? post.comments.length : 0} {(post.comments && post.comments.length == 1) ? t("Comment") : t("Comments") }</p>
+                              </div>
+                            </article>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+                  );
+                })
+              ) : (
+                <p>{t("This user has not posts")}</p>
+              )}
+            </div>
+          </div>
+          <div className="column is-2">
+            <ul className="lateral-menu">
+              <li><button className="btn" onClick={handleClickButtonNewPost}>{t("New Post")}</button></li>
+              <li className="lateral-menu__item" onClick={handleClickAllPosts}>{t("All posts")}</li>
+              <hr />
+              {(tags) ? (
+                tags.map((tag, index) => {
+                  return (<li key={index} className="tag" onClick={() => handleClickTag(tag)}>{tag.tag_name}</li>);
+                })
+              ) : (
+                null
+              )}
+            </ul>
+          </div>
         </div>
       </div>
-    </React.Fragment>
+    </section>
   );
 };
 
