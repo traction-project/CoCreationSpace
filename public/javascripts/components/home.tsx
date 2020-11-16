d439376e2d6296a7a085c8f6568ffc4a21c42f42
Thomas Röggla <tom@lichtfragmente.com>
2020-11-16 01:05:49 +0100

    replaced link component with history.push()

--- public/javascripts/components/home.tsx
+++ public/javascripts/components/home.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   const { t } = useTranslation();
+  const history = useHistory();
+
+  const navigateTo = (destination: string) => {
+    return () => {
+      history.push(destination);
+    };
+  };
 
   return (
     <section className="hero is-fullheight-with-navbar">
@@ -21,20 +28,14 @@ const Home: React.FC<HomeProps> = () => {
           <div className="columns is-centered is-mobile">
             <div className="column is-5-desktop is-6-tablet is-10-mobile">
               <ul className="home__menu">
-                <li className="home__menu-item">
-                  <Link to={"userPosts"}>
-                    {t("My Posts")}
-                  </Link>
+                <li className="home__menu-item" onClick={navigateTo("/userPosts")}>
+                  {t("My Posts")}
                 </li>
-                <li className="home__menu-item">
-                  <Link to={"posts"}>
-                    {t("Explore")}
-                  </Link>
+                <li className="home__menu-item" onClick={navigateTo("/posts")}>
+                  {t("Explore")}
                 </li>
-                <li className="home__menu-item">
-                  <Link to={"upload"}>
-                    {t("Create")}
-                  </Link>
+                <li className="home__menu-item" onClick={navigateTo("/upload")}>
+                  {t("Create")}
                 </li>
               </ul>
             </div>
