2c51d5070d4b4f2611a9451ded1b66a8fb0ba5f8
Thomas Röggla <tom@lichtfragmente.com>
2020-11-16 03:53:33 +0100

    cleaned up PostList component

--- public/javascripts/components/post_list/post_entry.tsx
+++ public/javascripts/components/post_list/post_entry.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { PostType } from "../post";
 import { useTranslation } from "react-i18next";
 
@@ -12,47 +12,51 @@ interface PostEntryProps {
 const PostEntry: React.FC<PostEntryProps> = (props) => {
   const { post } = props;
   const { t } = useTranslation();
+  const history = useHistory();
+
+  const navigateTo = (destination: string) => {
+    return () => {
+      history.push(destination);
+    };
+  };
 
   return (
-    <div>
-      <Link to={`/post/${post.id}`}>
-        <div className="list-item">
-          <div className="box" style={{width: "100%"}}>
-            <article className="media">
-              <UserLogo user={post.user}></UserLogo>
-              <div className="media-content">
-                <div className="content">
-                  <p>
-                    <strong className="post-title">{post.title ? post.title : "Post"}</strong>
-                    <small className="list-item__date">{post.createdAt && new Date(post.createdAt).toLocaleDateString()}</small>
-                    <br />
-                    <br />
-                    {post.dataContainer?.text_content}
-                  </p>
-                </div>
-                { (post.dataContainer && post.dataContainer.multimedia && post.dataContainer.multimedia.length > 0) ?
-                  <div className="list-item__files">
-                    {post.dataContainer.multimedia.map((multimedia, index) => {
-                      return (
-                        <div key={index}>
-                          <figure className="image is-24x24 list-item__files-item">
-                            <img src="/images/docs.png" />
-                          </figure>
-                        </div>
-                      );
-                    })}
-                  </div>
-                  : null}
-              </div>
-              <div className="media-right number-comments">
-                <i className="far fa-comment"></i>
-                <p>{(post.comments && post.comments.length > 0) ? post.comments.length : 0} {(post.comments && post.comments.length == 1) ? t("Comment") : t("Comments") }</p>
-              </div>
-            </article>
+    <article className="media is-clickable" onClick={navigateTo(`/post/${post.id}`)}>
+      <UserLogo user={post.user} />
+
+      <div className="media-content">
+        <div className="content">
+          <div className="block">
+            <strong className="post-title">{post.title ? post.title : "Post"}</strong>
+            <small className="list-item__date">{post.createdAt && new Date(post.createdAt).toLocaleDateString()}</small>
+          </div>
+
+          <div className="block">
+            <p>{post.dataContainer?.text_content}</p>
           </div>
         </div>
-      </Link>
-    </div>
+        {(post.dataContainer && post.dataContainer.multimedia && post.dataContainer.multimedia.length > 0) && (
+          <div className="list-item__files">
+            {post.dataContainer.multimedia.map((multimedia, index) => {
+              return (
+                <div key={index}>
+                  <figure className="image is-24x24 list-item__files-item">
+                    <img src="/images/docs.png" />
+                  </figure>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      <div className="media-right number-comments">
+        <i className="far fa-comment"></i>
+        <p>
+          {(post.comments && post.comments.length > 0) ? post.comments.length : 0}&nbsp;
+          {(post.comments && post.comments.length == 1) ? t("Comment") : t("Comments") }
+        </p>
+      </div>
+    </article>
   );
 };
 
