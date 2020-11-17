74c6c49d06d5f68bd0089393cd5d2f0126403df8
Thomas Röggla <tom@lichtfragmente.com>
2020-11-17 03:22:51 +0100

    moved toolbar below video into its own columns element

--- public/javascripts/components/post/post.tsx
+++ public/javascripts/components/post/post.tsx
@@ -248,61 +248,63 @@ const Post: React.FC<PostProps> = (props) => {
                 );
               })
             }
+          </div>
+        </div>
 
-            <div className="block">
-              <nav className="level is-mobile" style={{position: "relative"}}>
-                <div className="level-left">
-                  {(post.dataContainer?.multimedia && post.dataContainer?.multimedia.length > 0) && (
-                    <Fragment>
-                      <div className={`emoji-container ${showEmojis ? "" : "hidden"}`}>
-                        {emojis.map((emoji, index) => {
-                          return (
-                            <button key={index} className="emoji-item" onClick={() => handleClickEmojiItem(emoji)}>{emoji}</button>
-                          );
-                        })}
-                      </div>
+        <div className="columns">
+          <div className="column">
+            <nav className="level is-mobile" style={{position: "relative"}}>
+              <div className="level-left">
+                {(post.dataContainer?.multimedia && post.dataContainer?.multimedia.length > 0) && (
+                  <Fragment>
+                    <div className={`emoji-container ${showEmojis ? "" : "hidden"}`}>
+                      {emojis.map((emoji, index) => {
+                        return (
+                          <button key={index} className="emoji-item" onClick={() => handleClickEmojiItem(emoji)}>{emoji}</button>
+                        );
+                      })}
+                    </div>
 
-                      <a className="level-item button is-info is-small" onClick={handleClickEmojiButton}>
+                    <a className="level-item button is-info is-small" onClick={handleClickEmojiButton}>
+                      <span className="icon is-small">
+                        <i className="fas fa-smile"></i>
+                      </span>
+                    </a>
+
+                    {(!post.parent_post_id) && (
+                      <a className="level-item" onClick={(e) => openPortal(e)}>
                         <span className="icon is-small">
-                          <i className="fas fa-smile"></i>
+                          <i className="fas fa-language"/>
                         </span>
                       </a>
+                    )}
 
-                      {(!post.parent_post_id) && (
-                        <a className="level-item" onClick={(e) => openPortal(e)}>
-                          <span className="icon is-small">
-                            <i className="fas fa-language"/>
-                          </span>
-                        </a>
-                      )}
+                    {isOpen && (
+                      <Portal>
+                        <TranslationModal
+                          id={post.dataContainer.multimedia[0].id!}
+                          onSuccess={handleTranslationSuccess}
+                          onClose={closePortal}
+                        />
+                      </Portal>
+                    )}
+                  </Fragment>
+                )}
 
-                      {isOpen && (
-                        <Portal>
-                          <TranslationModal
-                            id={post.dataContainer.multimedia[0].id!}
-                            onSuccess={handleTranslationSuccess}
-                            onClose={closePortal}
-                          />
-                        </Portal>
-                      )}
-                    </Fragment>
-                  )}
+                <a className="level-item" onClick={handleClickReply}>
+                  <span className="icon is-small"><i className="fas fa-reply"></i></span>
+                </a>
 
-                  <a className="level-item" onClick={handleClickReply}>
-                    <span className="icon is-small"><i className="fas fa-reply"></i></span>
-                  </a>
-
-                  <a className="level-item" onClick={handleClickLike}>
-                    <span key="unique" className="icon is-small">
-                      <i className={ isLike ? "fas fa-heart" : "far fa-heart"} />
-                    </span>
-                  </a>
-                  <span className="level-item">
-                    {likes}
+                <a className="level-item" onClick={handleClickLike}>
+                  <span key="unique" className="icon is-small">
+                    <i className={ isLike ? "fas fa-heart" : "far fa-heart"} />
                   </span>
-                </div>
-              </nav>
-            </div>
+                </a>
+                <span className="level-item">
+                  {likes}
+                </span>
+              </div>
+            </nav>
           </div>
         </div>
 
