e3497c23b8e28482142ce16786ab2d392ca032a2
Thomas Röggla <tom@lichtfragmente.com>
2020-11-20 01:48:05 +0100

    added component to display posts with their thumbnails

---
+++ public/javascripts/components/post_list/post_thumbnail_entry.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import { PostType } from "../post/post";
+import UserLogo from "../user_logo";
+import Thumbnail from "../thumbnail";
+
+interface PostThumbnailEntryProps {
+  post: PostType;
+}
+
+const PostThumbnailEntry: React.FC<PostThumbnailEntryProps> = (props) => {
+  const { post } = props;
+  const { t } = useTranslation();
+  const history = useHistory();
+
+  const navigateTo = (destination: string) => {
+    return () => {
+      history.push(destination);
+    };
+  };
+
+  return (
+    <article className="media is-clickable" onClick={navigateTo(`/post/${post.id}`)}>
+      <UserLogo user={post.user} />
+
+      <div className="media-content">
+        <div className="content">
+          <strong className="post-title">{post.title ? post.title : t("Post")}</strong>
+          <small className="list-item__date">{post.createdAt && new Date(post.createdAt).toLocaleDateString()}</small>
+
+          <div className="m-4" style={{ display: "flex", flexWrap: "wrap" }}>
+            {(post.dataContainer?.multimedia && post.dataContainer.multimedia.length > 0) && (
+              post.dataContainer.multimedia.map(({ id, type }) => {
+                return (
+                  <Thumbnail key={id} id={id} type={type} />
+                );
+              })
+            )}
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default PostThumbnailEntry;
