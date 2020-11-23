b46cd4af6a0c28a80aadb5bdc8b34ed06f4af473
Thomas Röggla <tom@lichtfragmente.com>
2020-11-23 23:48:22 +0100

    deleting all users except for developers in /resetdatabase

--- routes/index.ts
+++ routes/index.ts
@@ -132,9 +132,24 @@ router.get("/resetdatabase", async (_, res) => {
     }
   });
 
+  const numDeletedUsers = await Users.destroy({
+    where: {
+      id: {
+        $notIn: [
+          "1dd4713c-6644-4257-bc81-1747b8f42437",
+          "c90d4627-c634-4825-8a97-979b2c350468",
+          "e77c1a35-c8bb-4ba5-b3b0-38e7fbae6805",
+          "b43590f5-847a-46c0-bcaf-970f69eea810",
+          "308fdc59-edc2-492f-857b-6e8e6c7f6c22 "
+        ]
+      }
+    }
+  });
+
   return res.send({
     status: "OK",
     deletedRecords: {
+      users: numDeletedUsers,
       posts: numDeletedPosts
     }
   });
