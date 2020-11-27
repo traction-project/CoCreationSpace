c9ceea6d7e582a9e28876c5f7d65dbe7c403d203
Thomas Röggla <tom@lichtfragmente.com>
2020-11-27 08:08:21 +0100

    removed /resetdatabase route

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import passport from "passport";
 import { readFileSync } from "fs";
-import { Op } from "sequelize";
 
 import APIRouter from "./api";
 import SNSRouter from "./sns";
@@ -103,78 +102,6 @@ router.get("/revision", (_, res) => {
   } catch {
     res.send("unknown");
   }
-});
-
-/**
- * Reset database to a pristine state for the trial.
- *
- * XXX THIS NEEDS TO BE DISABLED AFTER THE TRIAL!
- */
-router.get("/resetdatabase", async (req, res) => {
-  const { Posts, Users, EmojiReactions } = db.getModels();
-  const isDryRun = req.query.confirm == undefined;
-
-  const postQuery = {
-    where: {
-      id: {
-        [Op.notIn]: [
-          "d921b0c7-5166-4643-bd2f-353eb7a9bc95",
-          "91f34bf4-ce9e-4e39-9987-87c67c9b6acb",
-          "79710032-afa8-4f49-87d6-056af56eb95b",
-          "bf0931a3-6e07-4680-8e14-391cd91008a2",
-          "c52f131d-919f-4e01-92dd-c20873aa4b9d",
-          "0d8e32e9-6b7d-4dc6-ab54-8dff018502ec",
-          "6ec6e300-0f34-4f4f-8d09-7480fb7909d6",
-          "5fd0a6ee-40ce-4a18-8018-6604e94d5cad",
-          "b40de631-173f-4525-a1c1-09d81efa8569",
-          "3f6ddbea-dc2e-47da-a29b-d261f573e40e",
-          "2482d38c-536f-4ed1-a00e-4b2ea2ac748a"
-        ]
-      }
-    }
-  };
-
-  const userQuery = {
-    where: {
-      id: {
-        [Op.notIn]: [
-          "1dd4713c-6644-4257-bc81-1747b8f42437",
-          "c90d4627-c634-4825-8a97-979b2c350468",
-          "e77c1a35-c8bb-4ba5-b3b0-38e7fbae6805",
-          "b43590f5-847a-46c0-bcaf-970f69eea810",
-          "308fdc59-edc2-492f-857b-6e8e6c7f6c22"
-        ]
-      }
-    }
-  };
-
-  const emojiReactionQuery = {
-    where: {
-      post_id: "3f6ddbea-dc2e-47da-a29b-d261f573e40e"
-    }
-  };
-
-  let numDeletedPosts = 0, numDeletedUsers = 0, numDeletedEmojiReactions = 0;
-
-  if (isDryRun) {
-    numDeletedPosts = await Posts.count(postQuery);
-    numDeletedUsers = await Users.count(userQuery);
-    numDeletedEmojiReactions = await EmojiReactions.count(emojiReactionQuery);
-  } else {
-    numDeletedPosts = await Posts.destroy(postQuery);
-    numDeletedUsers = await Users.destroy(userQuery);
-    numDeletedEmojiReactions = await EmojiReactions.destroy(emojiReactionQuery);
-  }
-
-  return res.send({
-    status: "OK",
-    isDryRun,
-    deletedRecords: {
-      users: numDeletedUsers,
-      posts: numDeletedPosts,
-      emojiReactions: numDeletedEmojiReactions
-    }
-  });
 });
 
 export default router;
