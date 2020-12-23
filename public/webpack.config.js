e56c627ab01a5221be2ed8be7519459ffde5bf86
Thomas Röggla <t.roggla@cwi.nl>
2020-12-23 19:39:02 +0100

    added polyfill for node's buffer object

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -12,6 +12,7 @@ const frontend = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     fallback: {
+      "buffer": require.resolve("buffer"),
       "stream": require.resolve("stream-browserify")
     }
   },
