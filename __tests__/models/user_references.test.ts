dc5a61048467b9498bae56856f1f4f8f8e556045
hrivas23 <hrivas@vicomtech.org>
2021-02-16 13:28:58 +0100

    Added user references model tests

---
+++ __tests__/models/user_references.test.ts
@@ -0,0 +1,61 @@
+import { Sequelize } from "sequelize";
+
+process.env["SESSION_SECRET"] = "sessionsecret";
+const sequelize = new Sequelize("sqlite::memory:", { logging: false });
+
+import { db } from "../../models";
+
+describe("User references user tests", () => {
+  beforeAll(async () => {
+    await db.createDB(sequelize);
+  });
+
+  beforeEach(async () => {
+    const { UserReferences, Users, Posts } = db.getModels();
+
+    await UserReferences.destroy({ truncate: true });
+    await Users.destroy({ truncate: true });
+    await Posts.destroy({ truncate: true });
+  });
+
+  it("should be user1 referenced by post1", async () => {
+    const { Posts, Users } = db.getModels();
+    const user1 = await Users.create({ username: "user1" });
+    const post1 = await Posts.create({ title: "New post" });
+
+    expect(await post1.hasUserReferenced([user1])).toBeFalsy();
+    await post1.addUserReferenced(user1);
+    expect(await post1.hasUserReferenced([user1])).toBeTruthy();
+    expect(await user1.hasPostReferenced([post1])).toBeTruthy();
+  });
+
+  it("should be user1 referenced by post1 and post2", async () => {
+    const { Posts, Users } = db.getModels();
+    const user1 = await Users.create({ username: "user1" });
+    const post1 = await Posts.create({ title: "New post" });
+    const post2 = await Posts.create({ title: "Media post" });
+
+    expect(await post1.hasUserReferenced([user1])).toBeFalsy();
+    expect(await post2.hasUserReferenced([user1])).toBeFalsy();
+    await post1.addUserReferenced(user1);
+    await post2.addUserReferenced(user1);
+    expect(await post1.hasUserReferenced([user1])).toBeTruthy();
+    expect(await post2.hasUserReferenced([user1])).toBeTruthy();
+    expect(await user1.hasPostReferenced([post1, post2])).toBeTruthy();
+  });
+
+  it("should post1 remove reference to user1", async () => {
+    const { Posts, Users } = db.getModels();
+    const user1 = await Users.create({ username: "user1" });
+    const post1 = await Posts.create({ title: "New post" });
+
+    await post1.addUserReferenced(user1);
+    expect(await post1.hasUserReferenced([user1])).toBeTruthy();
+    expect(await user1.hasPostReferenced([post1])).toBeTruthy();
+
+    await post1.removeUserReferenced([user1]);
+    expect(await post1.hasUserReferenced([user1])).toBeFalsy();
+    expect(await user1.hasPostReferenced([post1])).toBeFalsy();
+  });
+
+});
