6e29d4c4a9d286487b23ea0ab0ea7aa8cb0c8400
Thomas Röggla <tom@lichtfragmente.com>
2021-02-20 21:39:00 +0100

    added test cases for SearchQuery model

---
+++ __tests__/models/search_query.test.ts
@@ -0,0 +1,53 @@
+import { Sequelize } from "sequelize";
+
+process.env["SESSION_SECRET"] = "sessionsecret";
+const sequelize = new Sequelize("sqlite::memory:", { logging: false });
+
+import { db } from "../../models";
+
+describe("SearchQuery model", () => {
+  beforeAll(async () => {
+    await db.createDB(sequelize);
+  });
+
+  beforeEach(async () => {
+    const { SearchQuery } = db.getModels();
+    await SearchQuery.destroy({ truncate: true });
+  });
+
+  it("should create a new instance", async () => {
+    const { SearchQuery } = db.getModels();
+
+    const query = await SearchQuery.create({
+      query: "test"
+    });
+
+    expect(query).toBeDefined();
+
+    expect(query.id).toBeDefined();
+    expect(query.createdAt).toBeDefined();
+    expect(query.updatedAt).toBeDefined();
+
+    expect(query.user).not.toBeDefined();
+
+    expect(query.createdAt).toEqual(query.updatedAt);
+    expect(query.query).toEqual("test");
+  });
+
+  it("should associate a new instance to a user", async () => {
+    const { SearchQuery, Users } = db.getModels();
+
+    const query = await SearchQuery.create({
+      query: "test"
+    });
+
+    const user = await Users.create({
+      username: "admin"
+    });
+
+    expect(await query.getUser()).toBeNull();
+
+    await query.setUser(user);
+    expect(await query.getUser()).toBeDefined();
+  });
+});
