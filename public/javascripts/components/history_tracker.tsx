9df939662977478c664a4d7869786026c6610860
Thomas Röggla <tom@lichtfragmente.com>
2021-02-21 14:21:46 +0100

    connected HistoryTracker to the Redux store to access login information

--- public/javascripts/components/history_tracker.tsx
+++ public/javascripts/components/history_tracker.tsx
@@ -1,17 +1,27 @@
 import * as React from "react";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
 
-interface HistoryTrackerProps {
+import { ApplicationState } from "../store";
+import { LoginState } from "../reducers/login";
+
+interface HistoryTrackerConnectedProps {
+  login: LoginState;
+}
+
+interface HistoryTrackerProps extends HistoryTrackerConnectedProps {
   endpoint: string;
+  children?: React.ReactNode;
 }
 
 const HistoryTracker: React.FC<HistoryTrackerProps> = (props) => {
+  const { children, login } = props;
   const history = useHistory();
 
   useEffect(() => {
     const removeListener = history.listen((location, action) => {
-      console.log(location, action);
+      console.log(location, action, login);
     });
 
     return () => {
@@ -21,9 +31,19 @@ const HistoryTracker: React.FC<HistoryTrackerProps> = (props) => {
 
   return (
     <>
-      {props.children}
+      {children}
     </>
   );
 };
 
-export default HistoryTracker;
+function mapStateToProps(state: ApplicationState): HistoryTrackerConnectedProps {
+  return {
+    login: state.login
+  };
+}
+
+function mapDispatchToProps() {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HistoryTracker);
