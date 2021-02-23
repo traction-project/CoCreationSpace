58fb54378f23cff1d5543aa3f0d982bbbf067990
Thomas Röggla <tom@lichtfragmente.com>
2021-02-23 16:00:02 +0100

    added unit tests for destroyWithComments() method

--- __tests__/models/post.test.ts
+++ __tests__/models/post.test.ts
@@ -43,4 +43,95 @@ describe("Posts model", () => {
 
     expect(await DataContainer.count()).toEqual(1);
   });
+
+  it("should retrieve all comments for a post", async () => {
+    const { Users, Posts } = db.getModels();
+
+    const post = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+    });
+
+    expect(await post.countComments()).toEqual(0);
+
+    const comment1 = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: post.id
+    });
+
+    expect(await post.countComments()).toEqual(1);
+    expect(await post.hasComment(comment1)).toBeTruthy();
+
+    const comment2 = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: post.id
+    });
+
+    expect(await post.countComments()).toEqual(2);
+    expect(await post.hasComments([comment1, comment2])).toBeTruthy();
+  });
+
+  it("should delete all comments if a post is deleted", async () => {
+    const { Users, Posts } = db.getModels();
+
+    const post = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+    });
+
+    const comment1 = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: post.id
+    });
+
+    const comment1Child = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: comment1.id
+    });
+
+    const comment2 = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: post.id
+    });
+
+    expect(await post.countComments()).toEqual(2);
+
+    await post.destroyWithComments();
+
+    expect(await Posts.findByPk(post.id)).toBeNull();
+    expect(await Posts.findByPk(comment1.id)).toBeNull();
+    expect(await Posts.findByPk(comment2.id)).toBeNull();
+    expect(await Posts.findByPk(comment1Child.id)).toBeNull();
+  });
+
+  it("should only delete comments which are children of the deleted post", async () => {
+    const { Users, Posts } = db.getModels();
+
+    const post = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+    });
+
+    const comment1 = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: post.id
+    });
+
+    const comment1Child = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: comment1.id
+    });
+
+    const comment2 = await Posts.create({
+      user_id: (await Users.findOne({}))!.id,
+      parent_post_id: post.id
+    });
+
+    expect(await post.countComments()).toEqual(2);
+
+    await comment1.destroyWithComments();
+
+    expect(await Posts.findByPk(post.id)).not.toBeNull();
+    expect(await Posts.findByPk(comment2.id)).not.toBeNull();
+
+    expect(await Posts.findByPk(comment1.id)).toBeNull();
+    expect(await Posts.findByPk(comment1Child.id)).toBeNull();
+  });
 });
