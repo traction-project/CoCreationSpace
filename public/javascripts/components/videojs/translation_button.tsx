a7191ec5d86ac77cf6711c5ec4cddc348a901dad
Thomas Röggla <t.roggla@cwi.nl>
2021-02-26 14:44:09 +0100

    removed video.js-related components

--- public/javascripts/components/videojs/translation_button.tsx
+++ public/javascripts/components/videojs/translation_button.tsx
@@ -1,38 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import usePortal from "react-useportal";
-import videojs, { VideoJsPlayer } from "video.js";
 
 import TranslationModal from "../post/translation_modal";
-import { activateSubtitleTrack, hasSubtitleTrack } from "../../util";
 
 interface TranslationButtonProps {
   videoId: string;
-  player: VideoJsPlayer;
   onTrackChanged?: (languaceCode: string) => void;
 }
 
 const TranslationButton: React.FC<TranslationButtonProps> = (props) => {
-  const { videoId, player, onTrackChanged } = props;
+  const { videoId, onTrackChanged } = props;
   const { isOpen, openPortal, closePortal, Portal } = usePortal();
 
   const handleTranslationSuccess = (languageCode: string, subtitleId: string) => {
     console.log("Translation success");
-
-    if (player) {
-      console.log("activating track");
-
-      if (!hasSubtitleTrack(player, languageCode)) {
-        player.addRemoteTextTrack({
-          kind: "subtitles",
-          srclang: languageCode,
-          src: `/media/subtitles/${subtitleId}`
-        }, true);
-      }
-
-      activateSubtitleTrack(player, languageCode);
-      onTrackChanged?.(languageCode);
-    }
+    onTrackChanged?.(languageCode);
   };
 
   return (
@@ -56,39 +38,4 @@ const TranslationButton: React.FC<TranslationButtonProps> = (props) => {
   );
 };
 
-const vjsComponent = videojs.getComponent("Component");
-
-class vjsTranslationButton extends vjsComponent {
-  private fnOnTrackChanged = (_: string) => {};
-
-  constructor(private playerInstance: videojs.Player, private videoId: string, options: videojs.ComponentOptions) {
-    super(playerInstance, options);
-    this.mount = this.mount.bind(this);
-
-    playerInstance.ready(() => {
-      this.mount();
-    });
-
-    this.on("dispose", () => {
-      ReactDOM.unmountComponentAtNode(this.el());
-    });
-  }
-
-  public onTrackChanged(fnOnTrackChanged: (languaceCode: string) => void) {
-    this.fnOnTrackChanged = fnOnTrackChanged;
-  }
-
-  mount() {
-    ReactDOM.render(
-      <TranslationButton
-        player={this.playerInstance}
-        videoId={this.videoId}
-        onTrackChanged={this.fnOnTrackChanged}
-      />,
-      this.el()
-    );
-  }
-}
-
-vjsComponent.registerComponent("vjsEpisodeList", vjsTranslationButton);
-export default vjsTranslationButton;
+export default TranslationButton;
