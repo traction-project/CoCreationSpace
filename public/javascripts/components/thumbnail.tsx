4805920759c8302b530bb58e2bcfc4a610c920f3
Thomas Röggla <tom@lichtfragmente.com>
2021-03-14 06:00:26 +0100

    rendering file icons for audio and other in file selection in Post

--- public/javascripts/components/thumbnail.tsx
+++ public/javascripts/components/thumbnail.tsx
@@ -11,26 +11,28 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ id, type, height = 100 }) => {
   const [ url, setUrl ] = useState<string>();
 
   useEffect(() => {
-    if (type == "video") {
+    switch (type) {
+    case "video":
       fetch(`/media/${id}/thumbnail`).then((res) => {
         return res.json();
       }).then((data) => {
         setUrl(data.thumbnail);
       });
-    } else {
+      break;
+    case "image":
       fetch(`/images/${id}`).then((res) => {
         return res.json();
       }).then((data) => {
         setUrl(data.thumbnail);
       });
+      break;
+    case "audio":
+      setUrl("/images/file-audio-solid.png");
+      break;
+    default:
+      setUrl("/images/file-file-solid.png");
     }
   }, []);
-
-  if (!url) {
-    return (
-      <div style={{ width: height, height, backgroundColor: "black", padding: "0.25rem" }} />
-    );
-  }
 
   return (
     <img src={url} style={{ height, padding: "0.25rem" }} />
