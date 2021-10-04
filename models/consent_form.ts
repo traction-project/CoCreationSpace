366a7b2643acfcc632e13be0a9fc5d9bc367d883
Thomas Röggla <tom@lichtfragmente.com>
2021-10-04 07:51:56 +0200

    added model for consent forms

---
+++ models/consent_form.ts
@@ -0,0 +1,64 @@
+import Sequelize, { Optional } from "sequelize";
+import { v4 as uuidv4 } from "uuid";
+
+import { CommonAttributes } from "util/typing/modelCommonAttributes";
+import { UserInstance, UsersAttributes } from "./users";
+
+export interface ConsentFormAttributes extends CommonAttributes {
+  user: UsersAttributes | UsersAttributes["id"];
+  data?: any;
+}
+
+type ConsentFormCreationAttributes = Optional<
+  ConsentFormAttributes,
+  "id" | "createdAt" | "updatedAt" | "user"
+>
+
+/**
+ * ConsentForm instance object interface
+ */
+export interface ConsentFormInstance extends Sequelize.Model<ConsentFormAttributes, ConsentFormCreationAttributes>, ConsentFormAttributes {
+  getUser: Sequelize.BelongsToGetAssociationMixin<UserInstance>;
+  setUser: Sequelize.BelongsToSetAssociationMixin<UserInstance, UserInstance["id"]>;
+  createUser: Sequelize.BelongsToCreateAssociationMixin<UserInstance>;
+}
+
+/**
+ * Build ConsentForm model object
+ *
+ * @param sequelize Sequelize: Conection object with de database
+ */
+export function ConsentFormModelFactory(sequelize: Sequelize.Sequelize): Sequelize.ModelCtor<ConsentFormInstance> {
+  //  DB table name
+  const TABLE_NAME = "consent_forms";
+
+  // Model attributtes
+  const attributes = {
+    id: {
+      type: Sequelize.DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.DataTypes.UUIDV4,
+      allowNull: false,
+      autoIncrement: false
+    },
+    data: {
+      type: Sequelize.DataTypes.JSON
+    },
+    created_at: {
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DataTypes.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false
+    }
+  };
+
+  // Create the model
+  const ConsentForm = sequelize.define<ConsentFormInstance, ConsentFormCreationAttributes>("internalNavigationStep", attributes, { underscored: true, tableName: TABLE_NAME });
+  ConsentForm.beforeCreate(group => { group.id = uuidv4(); });
+
+  return ConsentForm;
+}
