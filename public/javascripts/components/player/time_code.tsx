48efc1fe7408b22ba641b9cbdd3f073a892040f1
Thomas Röggla <t.roggla@cwi.nl>
2021-10-14 13:50:04 +0200

    TimeCode component now takes duration prop

--- public/javascripts/components/player/time_code.tsx
+++ public/javascripts/components/player/time_code.tsx
@@ -3,14 +3,17 @@ import { convertHMS } from "../../util";
 
 interface TimeCodeProps {
   time: number;
+  duration: number;
 }
 
 const TimeCode: React.FC<TimeCodeProps> = (props) => {
-  const { time } = props;
+  const { time, duration } = props;
 
   return (
     <span className="timecode">
       {convertHMS(time)}
+      &nbsp;/&nbsp;
+      {convertHMS(duration)}
     </span>
   );
 };
