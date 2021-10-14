b4dca11d228d440813487b98546582065d8740fc
Thomas Röggla <t.roggla@cwi.nl>
2021-10-14 14:08:57 +0200

    TimeCode component now also displays remaining time if code is clicked

--- public/javascripts/components/player/time_code.tsx
+++ public/javascripts/components/player/time_code.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { useState } from "react";
+
 import { convertHMS } from "../../util";
 
 interface TimeCodeProps {
@@ -8,17 +10,32 @@ interface TimeCodeProps {
 
 const TimeCode: React.FC<TimeCodeProps> = (props) => {
   const { time, duration } = props;
+  const [ displayMode, setDisplayMode ] = useState<"elapsed" | "remaining">("elapsed");
 
   return (
-    <span className="timecode">
-      {convertHMS(time)}
-      {(duration && !Number.isNaN(duration)) ? (
+    <span className="timecode" onClick={() => setDisplayMode((displayMode == "elapsed") ? "remaining" : "elapsed")}>
+      {(displayMode == "elapsed") ? (
         <>
-          &nbsp;/&nbsp;
-          {convertHMS(duration)}
+          {convertHMS(time)}
+          {(duration && !Number.isNaN(duration)) ? (
+            <>
+              &nbsp;/&nbsp;
+              {convertHMS(duration)}
+            </>
+          ) : (
+            null
+          )}
         </>
       ) : (
-        null
+        <>
+          {(duration && !Number.isNaN(duration)) ? (
+            <>
+              -{convertHMS(duration - time)}
+            </>
+          ) : (
+            null
+          )}
+        </>
       )}
     </span>
   );
