3c083dae8e3db15f72f4ef83d42d5480460e3d15
Thomas Röggla <tom@lichtfragmente.com>
2021-10-27 18:16:14 +0200

    rendering user's posts in PublicProfile

--- public/javascripts/components/public_profile.tsx
+++ public/javascripts/components/public_profile.tsx
@@ -120,6 +120,45 @@ const PublicProfile: React.FC<PublicProfileProps> = (props) => {
         <div className="columns">
           <div className="column">
             <h5 className="title is-5">{t("Latest Posts")}</h5>
+
+            <div>
+              {user.posts.filter((p) => p.parent_post_id == null).map((post, index) => {
+                return (
+                  <article key={index} className="media is-clickable post-entry" onClick={navigateTo(`/post/${post.id}`)}>
+                    <div className="media-content">
+                      <div className="content">
+                        <strong className="post-title">{post.title ? post.title : t("Post")}</strong>
+                        <small className="list-item__date">
+                          {post.createdAt && new Date(post.createdAt).toLocaleDateString()}&emsp;
+                          {post.createdAt && new Date(post.createdAt).toLocaleTimeString()}&emsp;
+                          in <i>{post.thread.topic.userGroup.name}</i>
+                        </small>
+
+                        <p className="mt-2">
+                          <i className="far fa-comment" />&nbsp;
+                          {post.comments?.length || 0}&nbsp;
+                          {t("Comment", { count: post.comments?.length || 0 })}
+                        </p>
+                      </div>
+
+                      {(post.dataContainer && post.dataContainer.multimedia && post.dataContainer.multimedia.length > 0) && (
+                        <div className="list-item__files">
+                          {post.dataContainer.multimedia.map((m, index) => {
+                            return (
+                              <div key={index}>
+                                <figure className="image is-24x24 list-item__files-item mr-2">
+                                  <img src={`/images/file-${m.type}-solid.png`} />
+                                </figure>
+                              </div>
+                            );
+                          })}
+                        </div>
+                      )}
+                    </div>
+                  </article>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
