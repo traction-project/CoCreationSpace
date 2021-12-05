0005114ab24c9182c6a77750dfb3b4f05ad1ef22
Thomas Röggla <tom@lichtfragmente.com>
2021-12-05 02:21:28 +0100

    fetching collections from server and rendering them

--- public/javascripts/components/post/add_to_collection_modal.tsx
+++ public/javascripts/components/post/add_to_collection_modal.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+
+interface NoteCollection {
+  id: string;
+  name: string;
+}
 
 interface AddToCollectionModalProps {
   id: string;
@@ -9,19 +16,63 @@ interface AddToCollectionModalProps {
 
 const AddToCollectionModal: React.FC<AddToCollectionModalProps> = (props) => {
   const { onClose, onItemAdded } = props;
+  const { handleSubmit, register, reset, watch } = useForm();
   const { t } = useTranslation();
 
-  const onConfirm = async () => {
+  const [ collections, setCollections ] = useState<Array<NoteCollection>>([]);
+
+  useEffect(() => {
+    fetch("/notes/collections").then((res) => {
+      return res.json();
+    }).then((data) => {
+      setCollections(data);
+      reset({ "collection": "" });
+    });
+  }, []);
+
+  const onConfirm = handleSubmit(async () => {
     onItemAdded?.();
     onClose();
-  };
+  });
 
   return (
     <div className="modal is-active">
       <div className="modal-background" onClick={onClose} />
       <div className="modal-content">
         <div className="box">
-          <h4 className="title is-4">{t("Add to Collection")}</h4>
+          <h4 className="title is-4">{t("Add to Note Collection")}</h4>
+          <hr/>
+
+          <div className="field">
+            <div className="control">
+              <div className="select">
+                <select {...register("collection")}>
+                  <optgroup label={t("New")}>
+                    <option style={{ fontStyle: "italic" }} value="">
+                      {t("Create new collection")}
+                    </option>
+                  </optgroup>
+                  <optgroup label={t("Existing")}>
+                    {collections.map((c) => {
+                      return (
+                        <option key={c.id} value={c.id}>{c.name}</option>
+                      );
+                    })}
+                  </optgroup>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          {(watch("collection") == "") && (
+            <div className="field">
+              <label className="label">{t("Name")}</label>
+              <div className="control">
+                <input className="input" type="text" />
+              </div>
+            </div>
+          )}
+
           <hr/>
 
           <div className="field is-grouped pt-4">
