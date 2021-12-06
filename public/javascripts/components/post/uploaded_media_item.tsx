acfac18366e4cbe106aa794c6ddc830cd0a161bd
Thomas Röggla <t.roggla@cwi.nl>
2021-12-06 14:05:09 +0100

    UploadedMediaItem now displays DeleteIcon and has onDelete callback

--- public/javascripts/components/post/uploaded_media_item.tsx
+++ public/javascripts/components/post/uploaded_media_item.tsx
@@ -3,34 +3,46 @@ import * as React from "react";
 import MediaPlayer from "../media_player";
 import Image from "../image";
 import File from "../file";
+import DeleteIcon from "./delete_icon";
 
 interface UploadedMediaItemProps {
   id: string;
   type: string;
+  onDelete: (id: string) => void;
 }
 
-const UploadedMediaItem: React.FC<UploadedMediaItemProps> = ({ id, type }) => {
+const UploadedMediaItem: React.FC<UploadedMediaItemProps> = ({ id, type, onDelete }) => {
   const onBlurDetected = (isBlurry: boolean) => {
     console.log("image blurry:", isBlurry);
   };
 
-  if (type == "video") {
-    return (
-      <MediaPlayer id={id} />
-    );
-  } else if (type == "audio"){
-    return (
-      <MediaPlayer type="audio" id={id} />
-    );
-  } else if (type == "image" ){
-    return (
-      <Image id={id} onBlurDetected={onBlurDetected} />
-    );
-  } else {
-    return (
-      <File id={id} />
-    );
-  }
+  const renderMediaItem = () => {
+    if (type == "video") {
+      return (
+        <MediaPlayer id={id} />
+      );
+    } else if (type == "audio"){
+      return (
+        <MediaPlayer type="audio" id={id} />
+      );
+    } else if (type == "image" ){
+      return (
+        <Image id={id} onBlurDetected={onBlurDetected} />
+      );
+    } else {
+      return (
+        <File id={id} />
+      );
+    }
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      {renderMediaItem()}
+
+      <DeleteIcon onClick={() => onDelete(id)} />
+    </div>
+  );
 };
 
 export default UploadedMediaItem;
