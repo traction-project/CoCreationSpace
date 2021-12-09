26cb05f049d9cee861491a0b61ce1c18776ccaa2
Thomas Röggla <tom@lichtfragmente.com>
2021-12-09 02:24:33 +0100

    added icon for editing images

---
+++ public/javascripts/components/edit_image_icon.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+interface EditImageIconProps {
+  onClick: () => void;
+}
+
+const EditImageIcon: React.FC<EditImageIconProps> = ({ onClick }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 60, right: 15,
+        width: 30, height: 30,
+        borderRadius: 2,
+        backgroundColor: "#FFFFFF",
+        boxShadow: "1px 1px 3px 0px rgba(60,60,60,0.75)",
+        cursor: "pointer"
+      }}
+      onClick={onClick}
+    >
+      <p style={{ textAlign: "center", marginTop: 5 }}>
+        <i className="fas fa-pencil-alt" style={{ fontSize: 20 }} />
+      </p>
+    </div>
+  );
+};
+
+export default EditImageIcon;
