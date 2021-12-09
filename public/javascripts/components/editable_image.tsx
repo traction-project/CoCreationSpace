cbe98788af44ec1bf95a0b26cabef3a5dd1ad00c
Thomas Röggla <tom@lichtfragmente.com>
2021-12-09 04:47:42 +0100

    implemented drawing in EditableImage

--- public/javascripts/components/editable_image.tsx
+++ public/javascripts/components/editable_image.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { useEffect, useRef } from "react";
 
+type Coords = [x: number, y: number];
+
 interface EditableImageProps {
   imageUrl: string;
   dimensions: [number, number];
@@ -10,17 +12,60 @@ const EditableImage: React.FC<EditableImageProps> = ({ imageUrl, dimensions: [ w
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+
+    if (!canvas || !context) {
+      return;
+    }
+
     const image = new Image();
-
     image.onload = () => {
-      const context = canvasRef.current?.getContext("2d");
+      context.drawImage(image, 0, 0, width, height);
+    };
+    image.src = imageUrl;
 
-      if (context) {
-        context.drawImage(image, 0, 0, width, height);
-      }
+    let isDrawing = false;
+    let prevCoords: Coords;
+
+    const getMousePosition = (e: MouseEvent): Coords => {
+      const boundingRect = canvas.getBoundingClientRect();
+
+      return [
+        e.clientX - boundingRect.left,
+        e.clientY - boundingRect.top
+      ];
     };
 
-    image.src = imageUrl;
+    canvas.onmousedown = (e: MouseEvent) => {
+      isDrawing = true;
+      prevCoords = getMousePosition(e);
+    };
+
+    canvas.onmouseup = () => {
+      isDrawing = false;
+    };
+
+    canvas.onmousemove = (e: MouseEvent) => {
+      if (isDrawing) {
+        const [ x, y ] = getMousePosition(e);
+        const [ prevX, prevY ] = prevCoords;
+
+        console.log("drawing at:", x, y);
+
+        context.beginPath();
+        context.moveTo(prevX, prevY);
+        context.lineTo(x, y);
+
+        context.strokeStyle = "red";
+        context.lineWidth = 2;
+
+        context.stroke();
+        context.closePath();
+
+        prevCoords = [x, y];
+      }
+    };
   }, []);
 
   const wrapperStyle: React.CSSProperties = {
