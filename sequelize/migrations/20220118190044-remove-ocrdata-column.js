a247aafff5078ee400098ab38dfb2b88615990fa
Thomas Röggla <t.roggla@cwi.nl>
2022-01-18 20:02:48 +0100

    added migration to remove ocr_data column from media_items table

--- sequelize/migrations/20220118190044-remove-ocrdata-column.js
+++ sequelize/migrations/20220118190044-remove-ocrdata-column.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    queryInterface.removeColumn("media_items", "ocr_data");
+  },
+
+  down: async (queryInterface, Sequelize) => {
     queryInterface.addColumn("media_items", "ocr_data", {
       type: Sequelize.DataTypes.JSON,
       allowNull: true
     });
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    queryInterface.removeColumn("media_items", "ocr_data");
   }
 };
