48df40736a1feeed11561daea2dfb151149a77a5
Thomas Röggla <t.roggla@cwi.nl>
2022-01-19 23:04:38 +0100

    restructured PrivateRoute to work with react-router v6

--- public/javascripts/components/private_route.tsx
+++ public/javascripts/components/private_route.tsx
@@ -1,25 +1,20 @@
 import * as React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import store from "../store";
 
 interface PrivateRouteProps {
-  path: string;
 }
 
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, path, ...rest }) => {
+const PrivateRoute: React.FC<PrivateRouteProps> = () => {
   const isLoggedIn = (): boolean => {
     return store.getState().login.loggedIn;
   };
 
-  return (
-    <Route path={path} {...rest} render={() => (
-      isLoggedIn() ? (
-        children
-      ) : (
-        <Navigate to="/login" />
-      )
-    )}></Route>
-  );
+  if (isLoggedIn()) {
+    return <Outlet/>;
+  }
+
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
