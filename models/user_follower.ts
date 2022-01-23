5d639d259182605664786ea9c93e3089d43eda53
Thomas Röggla <t.roggla@cwi.nl>
2022-01-23 21:31:49 +0100

    added UserFollower association model

---
+++ models/user_follower.ts
@@ -0,0 +1,28 @@
+import Sequelize from "sequelize";
+
+export interface UserFollowerAttributes {
+}
+
+/**
+ * UserFollower instance object interface
+ */
+export interface UserFollowerInstance extends Sequelize.Model<UserFollowerAttributes>, UserFollowerAttributes {
+}
+
+/**
+ * Build UserFollower model object
+ *
+ * @param sequelize Sequelize: Conection object with de database
+ */
+export function UserFollowerModelFactory(sequelize: Sequelize.Sequelize): Sequelize.ModelCtor<UserFollowerInstance> {
+  //  DB table name
+  const TABLE_NAME = "user_followers";
+
+  // Model attributtes
+  const attributes = {};
+
+  // Create the model
+  const UserFollower = sequelize.define<UserFollowerInstance>("userFollower", attributes, { underscored: true, tableName: TABLE_NAME });
+
+  return UserFollower;
+}
