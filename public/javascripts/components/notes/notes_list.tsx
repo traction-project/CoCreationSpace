f7dd2018e622e016273b88147000822df143575a
Thomas Röggla <tom@lichtfragmente.com>
2022-01-24 04:51:43 +0100

    listing favourite posts in NotesList component

--- public/javascripts/components/notes/notes_list.tsx
+++ public/javascripts/components/notes/notes_list.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import classNames from "classnames";
 
 import Thumbnail from "../thumbnail";
-import { MultimediaItem } from "../post/post";
+import { MultimediaItem, PostType } from "../post/post";
 
 export interface NoteCollection {
   id: string;
@@ -21,13 +21,20 @@ const NotesList: React.FC<NotesListProps> = (props) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const [ notes, setNotes ] = useState<Array<NoteCollection>>([]);
+  const [ notes, setNotes ] = useState<Array<NoteCollection>>();
+  const [ posts, setPosts ] = useState<Array<PostType>>();
 
   useEffect(() => {
     fetch("/notes/collections").then((res) => {
       return res.json();
     }).then((data) => {
       setNotes(data);
+    });
+
+    fetch("/users/favourites").then((res) => {
+      return res.json();
+    }).then((data) => {
+      setPosts(data);
     });
   }, []);
 
@@ -42,16 +49,64 @@ const NotesList: React.FC<NotesListProps> = (props) => {
       <div className="container">
         <div className="columns is-centered">
           <div className="column is-9">
-            <h4 className="title is-4">{t("Favourites")}</h4>
+            <h4 className="title is-4">{t("My Favourites")}</h4>
             <hr/>
 
-            {notes.map(({ id, name, mediaItems }) => {
+            {(posts?.length == 0) && (
+              <p>{t("You have no favourite posts yet")}</p>
+            )}
+
+            {posts?.map((post, index) => {
+              return (
+                <article key={index} className="media is-clickable post-entry" onClick={navigateTo(`/post/${post.id}`)}>
+                  <div className="media-content">
+                    <div className="content">
+                      <strong className="post-title">{post.title ? post.title : t("Post")}</strong>
+                      <small className="list-item__date">
+                        {post.createdAt && new Date(post.createdAt).toLocaleDateString()}&emsp;
+                        {post.createdAt && new Date(post.createdAt).toLocaleTimeString()}&emsp;
+                        in <i>{post.thread?.topic?.userGroup?.name || "Unknown"}</i>
+                      </small>
+
+                      <p className="mt-2">
+                        <i className="far fa-comment" />&nbsp;
+                        {post.comments?.length || 0}&nbsp;
+                        {t("Comment", { count: post.comments?.length || 0 })}
+                      </p>
+                    </div>
+
+                    {(post.dataContainer && post.dataContainer.mediaItems && post.dataContainer.mediaItems.length > 0) && (
+                      <div className="list-item__files">
+                        {post.dataContainer.mediaItems.map((m, index) => {
+                          return (
+                            <div key={index}>
+                              <figure className="image is-24x24 list-item__files-item mr-2">
+                                <img src={`/images/file-${m.type}-solid.png`} />
+                              </figure>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                </article>
+              );
+            })}
+
+            <h4 style={{ marginTop: "2em" }} className="title is-4">{t("Collections")}</h4>
+            <hr/>
+
+            {(notes?.length == 0) && (
+              <p>{t("You haven't created any collections yet")}</p>
+            )}
+
+            {notes?.map(({ id, name, mediaItems }) => {
               const mediaItemCount = mediaItems.length;
 
               return (
                 <article
                   key={id}
-                  className={classNames("media", "is-clickable", "p-4", "is-highlighted", "notes-list-entry")}
+                  className={classNames("media", "is-clickable", "p-4", "post-entry")}
                   onClick={navigateTo(`/note/${id}`)}
                 >
                   <figure className="media-left">
@@ -83,7 +138,6 @@ const NotesList: React.FC<NotesListProps> = (props) => {
                 </article>
               );
             })}
-
           </div>
         </div>
       </div>
