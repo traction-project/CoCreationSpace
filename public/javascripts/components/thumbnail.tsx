56dbd33d2c6a790250f070f7b764d11e5bc444a6
Thomas Röggla <tom@lichtfragmente.com>
2022-02-04 13:41:17 +0100

    thumbnail component optionally accepts filename to be rendered

--- public/javascripts/components/thumbnail.tsx
+++ public/javascripts/components/thumbnail.tsx
@@ -4,11 +4,12 @@ import { useState, useEffect } from "react";
 interface ThumbnailProps {
   id: string;
   type: string;
+  filename?: string
   height?: number;
   padding?: number | string;
 }
 
-const Thumbnail: React.FC<ThumbnailProps> = ({ id, type, height = 100, padding = "0.25rem" }) => {
+const Thumbnail: React.FC<ThumbnailProps> = ({ id, type, filename, height = 100, padding = "0.25rem" }) => {
   const [ url, setUrl ] = useState<string>();
 
   useEffect(() => {
@@ -34,6 +35,17 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ id, type, height = 100, padding =
       setUrl("/images/file-file-solid.png");
     }
   }, []);
+
+  if (filename && (type == "audio" || type == "file")) {
+    return (
+      <div style={{ height, width: height, padding }}>
+        <div style={{ textAlign: "center", overflow: "hidden", height: `calc(${height}px - 0.5rem)`, backgroundColor: "#E2E2E2", padding }}>
+          <img src={url} style={{ height: height / 2 }} />
+          <p className="is-size-7">{filename}</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <img src={url} style={{ height, padding }} />
