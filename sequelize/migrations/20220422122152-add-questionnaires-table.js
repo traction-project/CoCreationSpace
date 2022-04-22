b2c258fd224dbab913b0ddbd113860bc4b28ecdc
Thomas Röggla <t.roggla@cwi.nl>
2022-04-22 14:27:59 +0200

    questionnaire migration now also creates association table

--- sequelize/migrations/20220422122152-add-questionnaires-table.js
+++ sequelize/migrations/20220422122152-add-questionnaires-table.js
@@ -27,9 +27,37 @@ module.exports = {
         allowNull: false
       }
     });
+
+    queryInterface.createTable("user_questionnaires", {
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+      },
+      questionnaire_id: {
+        type: Sequelize.DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+      },
+      results: {
+        type: Sequelize.DataTypes.JSON,
+        allowNull: true
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    queryInterface.dropTable("video_chapters");
+    queryInterface.dropTable("user_questionnaires");
+    queryInterface.dropTable("questionnaires");
   }
 };
